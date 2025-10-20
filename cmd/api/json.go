352ae70dd0c68dd9a0209db33bc14149477095d9
Ikechukwu Iroka <martins.iroka@gmail.com>
2025-10-20 17:25:53 +0100

    added json marshal helper functions

---
+++ cmd/api/json.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func writeJSON(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	// to prevent dds or any form of injection, we need to set a maxBytes.
+	maxBytes := 1_048_578 // which represent 1mb
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	return decoder.Decode(data)
+}
+
+func writeJSONError(w http.ResponseWriter, status int, message string) error {
+	type envelope struct {
+		Error string `json:"error"`
+	}
+
+	return writeJSON(w, status, &envelope{message})
+}
